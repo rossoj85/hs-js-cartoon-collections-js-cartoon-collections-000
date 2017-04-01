64a2943f3163fc3a0319cd3408fc838c6f61207f
Jason  Rosso <jasonrosso@gmail.com>
2017-04-01 01:59:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+//1 Dwarf Roll Call
+/*test error? expects unecessary white space after last dwarf
+---> had to use .join("") and add in a white space at the end of push
+in order to manipulate the test
+rather than use .join(" ")*/
+
 function dwarfRollCall(dwarves) {
+  var array =[]
+  for (var i=0;i<dwarves.length;i++){
+    array.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return array.join("")
 }
 
+//2
 function summonCaptainPlanet(planeteerCalls){
+  var calls = planeteerCalls.map(a=>a.toUpperCase()+"!")
+  return calls
 }
 
 function longPlaneteerCalls(words) {
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i]
+  }
+}
+return "no cheese!"
 }
